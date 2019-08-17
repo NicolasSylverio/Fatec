49df9208118b83c3a83bf80382ea64cb884ec628
Nicolas Sylverio <nicolas_sylveriopereira@hotmail.com>
2019-08-17 00:24:44 -0300

    Adicionado estilo css as telas principais, Adicionado login e Ideneity

--- src/0-Presentation/Fatec.Mvc/App_Start/BundleConfig.cs
+++ src/0-Presentation/Fatec.Mvc/App_Start/BundleConfig.cs
@@ -1,11 +1,9 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Fatec.Mvc
 {
     public class BundleConfig
     {
-        // Para obter mais informações sobre o agrupamento, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,16 +12,14 @@ namespace Fatec.Mvc
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
-            // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                      "~/Scripts/materialize.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/materialize.min.css",
                       "~/Content/site.css"));
         }
     }
--- src/0-Presentation/Fatec.Mvc/Controllers/HomeController.cs
+++ src/0-Presentation/Fatec.Mvc/Controllers/HomeController.cs
@@ -11,15 +11,16 @@ namespace Fatec.Mvc.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult Privacy()
+        {
             return View();
         }
     }

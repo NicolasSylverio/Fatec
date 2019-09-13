10465c3ca37b8cc77b0bc441655f5ec05a0e7d76
Nicolas Sylverio <nicolaspereira@procobservicos.com.br>
2019-09-13 15:12:59 -0300

    Adicionadio Toast

--- src/0-Presentation/Fatec.Mvc/Scripts/site.js
+++ src/0-Presentation/Fatec.Mvc/Scripts/site.js
@@ -5,10 +5,10 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/service-worker.js')
-        .then(function(registration) {
+        .then(function (registration) {
             console.log('Registration successful, scope is:', registration.scope);
         })
         .catch(function (error) {
             console.log('Service worker registration failed, error:', error);
         });
-};
+}

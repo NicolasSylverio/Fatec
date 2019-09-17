22b8499597736d57a3aefd4c102deba117df4c92
Nicolas Sylverio <nicolas_sylveriopereira@hotmail.com>
2019-09-17 19:33:36 -0300

    Adicionado cadastro emprego

--- src/0-Presentation/Fatec.Mvc/Controllers/VagasEmpregoController.cs
+++ src/0-Presentation/Fatec.Mvc/Controllers/VagasEmpregoController.cs
@@ -1,4 +1,6 @@
 ﻿using Fatec.Application.Interface;
+using Fatec.Application.ViewModels;
+using System;
 using System.Web.Mvc;
 
 namespace Fatec.Mvc.Controllers
@@ -18,6 +20,80 @@ namespace Fatec.Mvc.Controllers
             ViewBag.Title = "Vagas de Emprego";
             var vagas = _vagaEmpregoAppService.GetAll();
             return View(vagas);
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        // GET: Tags/Edit/id
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var tag = _vagaEmpregoAppService.GetById(id);
+
+                return View(tag);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = $"Erro ao carregar Vaga. Erro: {ex.Message}";
+                return View();
+            }
+        }
+
+        // POST: Estagio/Edit/5
+        [HttpPost]
+        public ActionResult Edit(VagaEmpregoViewModel model)
+        {
+            try
+            {
+                _vagaEmpregoAppService.Update(model);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = $"Erro ao carregar Vaga. Erro: {ex.Message}";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(VagaEmpregoViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return View();
+
+                _vagaEmpregoAppService.Add(model);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Erro ao cadastrar nova Vaga Emprego";
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _vagaEmpregoAppService.Remove(id);
+
+                ViewBag.Sucess = "Vaga excluida com sucesso.";
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = $"Erro ao deletar Vaga. Erro: {ex.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
